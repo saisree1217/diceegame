035b6df52d89e675ac255283ba049d9ec0c884d9
saisree1217 <58566933+saisree1217@users.noreply.github.com>
2020-04-24 21:28:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomDiceImage="dice"+randomNumber1+".png";
+var randomImageSource="images/"+randomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randomDiceImage1="dice"+randomNumber2+".png";
+var randomImageSource1="images/"+randomDiceImage1;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource1);
+
+
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🥇Player 1 is winner!";
+}
+else if(randomNumber1<randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🥇Player 2 is winner!";
+}
+else if(randomNumber1===randomNumber2)
+{
+  document.querySelector("h1").innerHTML="😑Draw!";
+
+}
